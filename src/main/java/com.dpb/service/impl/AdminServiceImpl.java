d233e581b4d284035ab54c40ae071aaf7a795580
lzyooyle <1932096079@qq.com>
2020-09-17 13:40:33 +0800

    Create AdminServiceImpl.java

---
+++ src/main/java/com.dpb/service/impl/AdminServiceImpl.java
@@ -0,0 +1,26 @@
+package com.dpb.service.impl;
+
+import com.dpb.mapper.AdminMapper;
+import com.dpb.pojo.Admin;
+import com.dpb.pojo.Result;
+import com.dpb.service.AdminService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+    @Resource
+    private AdminMapper adminMapper;
+    @Override
+    public void insertAdmin(Admin admin){
+        adminMapper.insertAdmin(admin);
+    }
+    @Override
+    public Admin queryByName(String username){
+        return this.adminMapper.queryByName(username);
+    }
+}
