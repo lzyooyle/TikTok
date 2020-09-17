2ba35d23222a6fe1146db50cb9f7e96eb2695ad3
lzyooyle <1932096079@qq.com>
2020-09-17 13:41:59 +0800

    Create AdminService.java

---
+++ src/main/java/com.dpb/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.dpb.service;
+
+import com.dpb.pojo.Admin;
+import com.dpb.pojo.Result;
+
+public interface AdminService{
+    void insertAdmin(Admin admin);
+    Admin queryByName(String username);
+}
