daf2d556643f83e4169b8c2088e3b12d5297afa1
lzyooyle <1932096079@qq.com>
2020-10-28 13:48:46 +0800

    Create UserService.java

---
+++ spring-boot-paging-jpa/src/main/java/com/dpb/service/UserService.java
@@ -0,0 +1,23 @@
+package com.dpb.service;
+import com.dpb.pojo.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @ Author: keguang
+ * @ Date: 2019/7/18 10:26
+ * @ version: v1.0.0
+ * @ description:
+ */
+
+/**
+ * interface define methods
+ */
+public interface UserService{
+    Page<User> getUserList(int pageNum,int pageSize);
+    User findUserById(long id);
+    void save(User user);
+    void edit(User user);
+    void delete(long id);
+    boolean verifyLogin(User user);
+}
