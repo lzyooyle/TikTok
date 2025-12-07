7eeeb9536c697b6ca666d4d38bc2c9ca3e3198fc
lzyooyle <1932096079@qq.com>
2025-12-07 22:48:46 +0800

    Update 03-多线程第一种实现方式.java

--- Java 基础/黑马多线程/03-多线程第一种实现方式.java
+++ Java 基础/黑马多线程/03-多线程第一种实现方式.java
@@ -1,1 +1,19 @@
+package domain;
 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.setName("线程1");
+        t1.start();
+    }
+
+}
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(getName() + "Hello World");
+        }
+    }
+}
