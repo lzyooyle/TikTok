e4d3691e5a124d7025d8ac7b99a6fc6a72678eae
lzyooyle <1932096079@qq.com>
2025-12-14 17:09:36 +0800

    Update 03-多线程第一种实现方式.java

--- Java 基础/黑马多线程/03-多线程第一种实现方式.java
+++ Java 基础/黑马多线程/03-多线程第一种实现方式.java
@@ -3,6 +3,13 @@ package domain;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        //getName() 返回此线程的名字
+        //setName() 设置线程的名字
+        //细节：
+        //	1、如果我们没有给线程设置名字，线程也是有默认的名字的
+        //		格式：Thread-x（x序号，从0开始的）
+        //	2、如果我们要给线程池设置名字，可以用set方法进行设置，也可以用构造方法设置
+        
         MyThread t1 = new MyThread();
         t1.setName("线程1");
         t1.start();
